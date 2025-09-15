0638318ee684baa085b14203c7ba7792f97665ae
Jahnavig2526 <jahnavigurugubelli2504@gmail.com>
2025-09-15 19:07:50 +0530

    Initial commit for Gdf-project

--- user_dashboard.php
+++ user_dashboard.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-
-// Only allow logged-in users with user role (role_id = 2)
-if (!isset($_SESSION['role_id']) || $_SESSION['role_id'] != 2) {
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'user') {
     header("Location: login.php");
     exit();
 }
+$username = $_SESSION['username'];
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +13,14 @@ if (!isset($_SESSION['role_id']) || $_SESSION['role_id'] != 2) {
     <title>User Dashboard</title>
 </head>
 <body>
-    <h2>Welcome, User!</h2>
-    <p>You can upload and view files.</p>
-    <ul>
-        <li><a href="upload_file.php">Upload File</a></li>
-        <li><a href="view_files.php">View Files</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
+<h2>Welcome, <?php echo $username; ?>!</h2>
+
+<ul>
+    <li><a href="view_files.php">View Files</a></li>
+    <li><a href="upload_file.php">Upload Files</a></li>
+    <li><a href="download_file.php">Download Files</a></li>
+</ul>
+
+<a href="logout.php">Logout</a>
 </body>
 </html>
